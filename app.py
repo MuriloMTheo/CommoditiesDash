0ec4f65f69df36fd6f6d2111c38c4a47d132e000
MuriloMTheo <murilomoreira20123@hotmail.com>
2025-07-30 23:21:58 -0300

    ArrumandoAPI

--- app.py
+++ app.py
@@ -11,40 +11,44 @@ app = Flask(__name__)
 def coleta_dados():
     url = "https://br.investing.com/commodities/softs"
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers)
+    try:
+        response = requests.get(url, headers=headers, timeout=10)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, "html.parser")
 
-    soup = BeautifulSoup(response.content, "html.parser")
+        rows = soup.select("table tbody tr")
+        result = []
 
-    rows = soup.select("table tbody tr")
-    result = []
+        for row in rows:
+            columns = row.find_all("td")
 
-    for row in rows:
-        columns = row.find_all("td")
-        if len(columns) >= 8:
-            nome = columns[1].text.strip()
-            mes = columns[2].text.strip()
-            valorultimo = columns[3].text.strip()
-            valormax = columns[4].text.strip()
-            valormin = columns[5].text.strip()
-            variacao = columns[6].text.strip()
-            variacaoporcetagem = columns[7].text.strip()
-            hora = columns[8].text.strip()
+            if len(columns) >= 9:
+                nome = columns[1].text.strip()
+                mes = columns[2].text.strip()
+                valorultimo = columns[3].text.strip()
+                valormax = columns[4].text.strip()
+                valormin = columns[5].text.strip()
+                variacao = columns[6].text.strip()
+                variacaoporcetagem = columns[7].text.strip()
+                hora = columns[8].text.strip()
 
-            data_atual = datetime.now().strftime("%Y-%m-%d")
-            data_hora_coleta = f"{data_atual} {hora}"
+                data_atual = datetime.now().strftime("%Y-%m-%d")
+                data_hora_coleta = f"{data_atual} {hora}"
 
-            result.append({
-                "Nome": nome,
-                "DataHoraColeta": data_hora_coleta,
-                "Mes": mes,
-                "UltimoValor": valorultimo,
-                "ValorMaximo": valormax,
-                "ValorMinimo": valormin,
-                "Variacao": variacao,
-                "PorcentagemVariacao": variacaoporcetagem
-            })
-
-    return result
+                result.append({
+                    "Nome": nome,
+                    "DataHoraColeta": data_hora_coleta,
+                    "Mes": mes,
+                    "UltimoValor": valorultimo,
+                    "ValorMaximo": valormax,
+                    "ValorMinimo": valormin,
+                    "Variacao": variacao,
+                    "PorcentagemVariacao": variacaoporcetagem
+                })
+        return result
+    except Exception as e:
+        print(f"Erro ao coletar dados: {e}")
+        return []
 
 
 @app.route("/commodities")
@@ -57,8 +61,7 @@ def commodities():
     if filtro:
         filtro = filtro.lower().replace(" ", "")
         dados = [
-            item for item in dados if filtro in item["Nome"].lower().replace(" ", "")
-        ]
+            item for item in dados if filtro in item["Nome"].lower().replace(" ", "")]
     else:
         print("Nenhum filtro aplicado")
 
