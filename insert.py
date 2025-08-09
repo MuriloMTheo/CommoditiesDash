28a543ac9cbaa24ef1c6245f41b16550a1c62020
MuriloMTheo <murilomoreira20123@hotmail.com>
2025-08-09 18:45:09 -0300

    ArrumandoInsert

--- insert.py
+++ insert.py
@@ -1,48 +1,43 @@
+from scraping import coleta_dado
+from db import connect
 import psycopg2
-import requests
 import json
 import os
-from db import connect
 
-url = os.environ.get(
-    'API_URL', 'https://web-production-38e46.up.railway.app/commodities')
-response = requests.get(url)
-dados = response.json()
 
-conn = connect()
-cursor = conn.cursor()
+def insere_dado(dados):
+    conn = psycopg2.connect(
+        host=os.environ["DB_HOST"],
+        port=os.environ["DB_PORT"],
+        dbname=os.environ["DB_NAME"],
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"]
+    )
 
-if isinstance(dados, list) and all(isinstance(item, dict) for item in dados):
+    cursor = conn.cursor()
+
     for item in dados:
-        data_hora = item.get('DataHoraColeta')
-        if not data_hora:
-            continue
+        cursor.execute("""
+            INSERT INTO commodities 
+            (nome, data_hora, valor_atual, valor_maximo, valor_minimo, variacao, porcentagem_var, json_commodities)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """, (
+            item["Nome"],
+            item["DataHoraColeta"],
+            float(item["UltimoValor"].replace('.', '').replace(',', '.')),
+            float(item["ValorMaximo"].replace('.', '').replace(',', '.')),
+            float(item["ValorMinimo"].replace('.', '').replace(',', '.')),
+            float(item["Variacao"]),
+            float(item["PorcentagemVariacao"].replace('%', '')),
+            json.dumps(item)
+        ))
 
-        cursor.execute(
-            'SELECT COUNT(*) FROM commodities WHERE nome = %s AND data_hora = %s',
-            (item.get('Nome'), data_hora)
-        )
-        if cursor.fetchone()[0] == 0:
-            cursor.execute(
-                'INSERT INTO commodities (nome, data_hora, valor_atual, valor_maximo, valor_minimo, variacao, porcentagem_var, json_commodities) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)',
-                (
-                    item.get('Nome'),
-                    data_hora,
-                    float(item.get('UltimoValor', '0').replace(
-                        '.', '').replace(',', '.')),
-                    float(item.get('ValorMaximo', '0').replace(
-                        '.', '').replace(',', '.')),
-                    float(item.get('ValorMinimo', '0').replace(
-                        '.', '').replace(',', '.')),
-                    float(item.get('Variacao', '0').replace(',', '.')),
-                    item.get('PorcentagemVariacao', ''),
-                    json.dumps(item, ensure_ascii=False)
-                )
-            )
-else:
-    print("Erro: resposta da API não é uma lista de dicionários ou está vazia.")
-    print(dados)
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print("Sucesso")
 
-conn.commit()
-cursor.close()
-conn.close()
+
+if __name__ == "__main__":
+    dados = coleta_dado()
+    insere_dado(dados)
