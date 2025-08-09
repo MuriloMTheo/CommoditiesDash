6399562b8aaee780c56b0906855dbc4e5e7b2a6b
MuriloMTheo <murilomoreira20123@hotmail.com>
2025-08-09 18:23:40 -0300

    Teste

--- db.py
+++ db.py
@@ -4,4 +4,7 @@ import os
 
 def connect():
     dsn = os.getenv("DATABASE_URL")
+    print(f"DEBUG DATABASE_URL: {dsn}")
+    if not dsn:
+        raise Exception("DATABASE_URL não está definida no ambiente")
     return psycopg2.connect(dsn)
